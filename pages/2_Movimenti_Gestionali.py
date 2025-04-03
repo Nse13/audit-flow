3ff69d693b174f6ec6b51672392ac8ca9edf96aa
Nse13 <gobinomanuel@outlook.it>
2025-04-03 21:34:24 +0200

    Update 2_Movimenti_Gestionali.py

--- pages/2_Movimenti_Gestionali.py
+++ pages/2_Movimenti_Gestionali.py
@@ -1,3 +1,4 @@
-# Pagina movimenti contabili
 import streamlit as st
-st.title('Movimenti contabili')
+
+st.title("📂 Movimenti Gestionali")
+st.write("Carica fatture, movimenti bancari, cassa e gestisci le scritture contabili.")
